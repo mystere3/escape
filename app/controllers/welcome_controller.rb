d09d6b85b6864844ca8c570599e3c1b0dffc4d54
mystere3 <mystere3@msn.com>
2015-05-14 10:10:21 -0400

    adding welcome show page to test normal link

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@ class WelcomeController < ApplicationController
     if current_user
       redirect_to "/games"
     end
-    
   end
+
+  def show
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :welcome
   root 'welcome#index'
 
   # Example of regular route:
